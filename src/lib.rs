530428e78e6a95f9aad8ba5ba4d5bec59d3bbc71
Kevin Boos <kevinaboos@gmail.com>
2017-07-24 19:48:01 -0500

    Separated the returned values for QueuedData (for the producers) and PeekedData (for the consumer).

--- src/lib.rs
+++ src/lib.rs
@@ -22,55 +22,133 @@ extern crate collections;
 
 use spin::{Mutex, MutexGuard};
 use core::sync::atomic::{AtomicBool, Ordering};
+use core::ops::Deref;
 use alloc::arc::Arc;
 use collections::VecDeque;
 
 
 
-/// These two types indicate that only immutable items can be queued right now. 
-pub type EnqueueParam<T> = Arc<T>;
-type QueuedDataType<T> = Arc<QueuedData<T>>;
 
-
-/// A wrapper around the data that has been queued. This data type is returned when peeking into the queue. 
-/// private note:  This must NOT be cloned because it contains an inner Arc reference to the data passed to its constructor!
+/// A special reference type that wraps a data item that has been queued. 
+/// This is returned to a producer thread (the user of a DFQueueProducer)
+/// when enqueuing an item onto the queue so that the producer 
+/// can retain a reference to it in the case of failure.
 #[derive(Debug)]
 pub struct QueuedData<T> {
-    data: Arc<T>,
-    completed: AtomicBool, 
+    inner: Arc<InnerQueuedData<T>>,
 }
 
 impl<T> QueuedData<T> {
-    /// this does not take ownership of the given `dataref` Arc instance
-    /// (nor does it borrow it long term), 
-    /// allowing you as the caller to retain ownership of that data. 
-    fn new(dataref: Arc<T>) -> QueuedData<T> {
-        QueuedData {
-            data: dataref.clone(),
-            completed: AtomicBool::new(false),
-        }
-    }
 
-    /// Mark this item as completed. 
-    /// This allows the producer (the one that pushed it onto the queue) to know that it's safe to remove it from the queue. 
-    pub fn mark_completed(&self) {
-        self.completed.store(true, Ordering::SeqCst);
+    /// Not public, a DFQueueProducer must call an enqueue function to receive one of these back.
+    fn new(data: T) -> QueuedData<T> {
+        QueuedData {
+            inner: Arc::new(InnerQueuedData::new(data)),
+        }
     }
 
     /// Whether this item has been completed (handled) by the DFQueueConsumer.
     /// If an item is completed, the producer knows it's okay to remove it from the queue.
     pub fn is_completed(&self) -> bool {
-        self.completed.load(Ordering::SeqCst)
+        self.inner.completed.load(Ordering::SeqCst)
     }
 
-    // /// returns true if the given `QueuedData` was locked
-    // fn is_locked(&self) -> bool {
-    //     match self.data.try_lock() {
-    //         Some(_) => false,
-    //         None => true,
-    //     }
-    // }
+
+    /// Returns true if this data is still on the queue
+    /// 
+    /// The logic here is as follows: the thread invoking this function holds one reference. 
+    /// Thus, if there is more than one reference, then it means it is on the queue, 
+    /// because the queue also holds a reference to it. 
+    /// That's why we cannot call it internally --  it won't be correct because the original producer thread
+    /// may also be holding a reference to it, in order for that producer to retain a reference to it 
+    //// 
+    ///
+    /// Private note: do not call this internally! This is a public API meant for the producer thread to use.
+    pub fn is_enqueued(&self) -> bool {
+        Arc::strong_count(&self.inner) > 1
+    }
+
+
+    /// The logic here is as follows: if the item on the queue has only one reference, and has not been completed,
+    /// that means it is no longer on the queue (the consumer thread crashed or failed). 
+    /// Again, the producer thread invoking this function holds one reference, and if the data is indeed enqueued, 
+    /// that constitutes another reference. 
+    /// If the reference held in the queue isn't there (only one total), and the thread hasn't completed 
+    /// (it may have just been completed in another enqueue call), then something went wrong because it's not complete
+    /// and not on the queue anymore. 
+    /// 
+    /// Private note: do not call this internally! This is a public API meant for the producer thread to use.
+    pub fn has_failed(&self) -> bool {
+        // an item that has been completed could not have possibly failed!
+        (!self.is_completed()) && 
+        (Arc::strong_count(&self.inner) == 1)
+    }
+
+
+    /// creates a clone of this, marshalled as a PeekedData 
+    /// so that a consumer can access the InnerQueuedData without being able to access 
+    /// all of the QueuedData methods here.
+    fn as_peeked(&self) -> PeekedData<T> {
+        PeekedData {
+            inner: self.inner.clone(),
+        }
+    }
+
+    /// not public, because we want to control when this is cloned 
+    /// so we can keep track of the number of Arc references to the InnerQueuedData.
+    fn clone(&self) -> QueuedData<T> {
+        QueuedData {
+            inner: self.inner.clone(),
+        }
+    }
 }
+
+
+
+#[derive(Debug)]
+struct InnerQueuedData<T> {
+    data: T,
+    completed: AtomicBool, 
+}
+
+impl<T> InnerQueuedData<T> {
+
+    fn new(data: T) -> InnerQueuedData<T> {
+        InnerQueuedData {
+            data: data,
+            completed: AtomicBool::new(false),
+        }
+    }
+
+}
+
+
+/// A wrapper around data in the queue that allows a DFQueueConsumer 
+/// to access the data and mark the queued item as completed. 
+/// Automatically Derefs to the inner type `&T`, just like Arc does. 
+#[derive(Debug)]
+pub struct PeekedData<T> {
+    inner: Arc<InnerQueuedData<T>>,
+}
+
+impl<T> PeekedData<T> {
+
+    /// Mark this item as completed.
+    /// This allows the producer (the one that pushed it onto the queue) to know that it's safe to remove it from the queue.
+    pub fn mark_completed(&self) {
+        self.inner.completed.store(true, Ordering::SeqCst);
+    }
+
+}
+
+impl<T> Deref for PeekedData<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.inner.data
+    }
+}
+
 
 /// taken from Rust's VecDeque implementation
 const INITIAL_CAPACITY: usize = 7; // 2^3 - 1
@@ -81,7 +159,7 @@ const INITIAL_CAPACITY: usize = 7; // 2^3 - 1
 #[derive(Debug)]
 pub struct DFQueue<T> {
     /// the actual inner queue
-    queue: Mutex<VecDeque<QueuedDataType<T>>>,
+    queue: Mutex<VecDeque<QueuedData<T>>>,
     /// whether this queue has a consumer (it can only have one!!)
     has_consumer: AtomicBool,
 }
@@ -148,19 +226,19 @@ impl<T> DFQueueConsumer<T> {
     pub fn obtain_producer(&self) -> DFQueueProducer<T> {
         DFQueueProducer {
             qref: self.qref.clone(),
-            local_backlog: VecDeque::new(), // TODO: may need to wrap this in an Rc<>
+            local_backlog: VecDeque::new(), 
         }
     }
 
     
     /// internal function that returns the first non-completed item in the queue (queue is locked already)
-    fn peek_locked(&self, locked_queue: MutexGuard<VecDeque<QueuedDataType<T>>>) -> Option<QueuedDataType<T>> {
+    fn peek_locked(&self, locked_queue: MutexGuard<VecDeque<QueuedData<T>>>) -> Option<PeekedData<T>> {
         for e in locked_queue.iter() {
-            if e.completed.load(Ordering::SeqCst) {
+            if e.is_completed() {
                 continue;
             }
             else {
-                return Some(e.clone());
+                return Some(e.as_peeked());
             }
         }
 
@@ -175,8 +253,8 @@ impl<T> DFQueueConsumer<T> {
     /// or `None` if the queue is empty. 
     ///
     /// Note: this is NOT IRQ-safe. 
-    /// TODO: use a data guard to automatically release the lock to the returned QueuedData
-    pub fn peek_locking(&self) -> Option<QueuedDataType<T>> {
+    /// TODO: use a data guard to automatically release the lock to the returned InnerQueuedData
+    pub fn peek_locking(&self) -> Option<PeekedData<T>> {
         // first, acquire the lock so we have exclusive access to the inner queue
         let innerq = self.qref.queue.lock(); // blocking call
         
@@ -190,8 +268,8 @@ impl<T> DFQueueConsumer<T> {
     /// Returns `None` if the lock to the inner queue cannot be obtained, or if the queue is empty.
     ///
     /// Note: this is IRQ-safe. 
-    /// TODO: use a data guard to automatically release the lock to the returned QueuedData
-    pub fn peek_lockless(&self) -> Option<QueuedDataType<T>> {
+    /// TODO: use a data guard to automatically release the lock to the returned InnerQueuedData
+    pub fn peek_lockless(&self) -> Option<PeekedData<T>> {
         // first, try to acquire the queue lock. 
         let lock_result = self.qref.queue.try_lock();
         match lock_result {
@@ -213,7 +291,7 @@ impl<T> DFQueueConsumer<T> {
 #[derive(Debug)]
 pub struct DFQueueProducer<T> {
     qref: Arc<DFQueue<T>>,
-    local_backlog: VecDeque<QueuedDataType<T>>,
+    local_backlog: VecDeque<QueuedData<T>>,
 }
 
 
@@ -224,7 +302,7 @@ impl<T> DFQueueProducer<T> {
     pub fn obtain_producer(&self) -> DFQueueProducer<T> {
         DFQueueProducer {
             qref: self.qref.clone(),
-            local_backlog: VecDeque::new(), // TODO: may need to wrap this in an Rc<>
+            local_backlog: VecDeque::new(),
         }
     }
 
@@ -248,35 +326,56 @@ impl<T> DFQueueProducer<T> {
     }
 
 
-    /// pushes the given `QueuedData`, which is an Arc reference wrapping the actual data item, onto the queue.
+    /// pushes the given `data` onto the back of the queue.
     /// This is a blocking call that will acquire the queue lock, write the data, 
     /// and release the lock before returning to the caller. 
     ///
+    /// # Returns 
+    /// Returns a QueuedData instance, an Arc-like reference to the given `data` on the queue.
+    /// This ensures that the producer can still retain the given `data` if the queue experiences a failure. 
+    ///
     /// Note: this is NOT IRQ-safe. 
-    pub fn enqueue_locking(&mut self, dataref: EnqueueParam<T>) {
+    pub fn enqueue_locking(&mut self, data: T) -> QueuedData<T>{
 
         // acquire the lock so we have exclusive access to the inner queue
         let mut innerq = self.qref.queue.lock(); // blocking call
 
         // 1) move all the previously-backlogged data into the inner queue
-        // 2) push the actual `dataref` onto the queue
+        // 2) push the actual `data` onto the queue
         innerq.append(&mut self.local_backlog);
-        innerq.push_back(Arc::new(QueuedData::new(dataref.clone()))); 
 
+        let queued_data: QueuedData<T> = QueuedData::new(data);
+        innerq.push_back(queued_data.clone()); 
+
+        // TODO: remove items from the innerq that have been completed
+
+        queued_data
     }
 
 
-    /// pushes the given `QueuedData`, which is an Arc reference wrapping the actual data item,
-    /// onto the back of the queue, only if the queue lock can be acquired immediately.
-    /// If the queue lock cannot be acquired, then the given `QueuedData` is put on a local backlog queue 
-    /// that is specific to this `DFQueueRef`, which will be merged into the actual inner queue
+    /// pushes the given `data` onto the back of the queue, only if the queue lock can be acquired immediately.
+    /// If the queue lock cannot be acquired, then the given `data` is put on a local backlog queue 
+    /// that is specific to this `DFQueueProducer`, which will be merged into the actual inner queue
     /// on the next invocation of any `enqueue` method, provided that the inner queue can be locked immediately at that point. 
-    /// Returns true if the given `dataref` was added to the inner queue directly, false if it was added to the backlog.
+    ///
+    /// # Returns 
+    /// Returns a tuple of the form `(QueuedData, bool)` in which:
+    /// <ul> QueuedData is an Arc-like reference to the given `data` on the queue, which 
+    /// ensures that the producer can still retain the given `data` if the queue experiences a failure.
+    /// <ul> bool: true if the given `data` was added to the actual queue,
+    ///            false if it was placed into this `DFQueueProducer`'s backlog queue.
+    ///
+    /// # Important Note
+    /// if the returned boolean is false, the data was placed on the backlog queue and 
+    /// IS NOT GUARANTEED TO BE ON THE ACTUAL QUEUE until [`flush_backlog`]: #method.flush_backlog is called.
+    /// Note that calling [`enqueue_locking`]: #method.enqueue_locking will cause the backlog to be flushed to the actual queue.
+    /// If `enqueue_lockless` is called again, the backlogged data MAY be added to the actual queue, but that is also not guaranteed!
+    /// 
     ///
     /// Note: this is IRQ-safe. 
-    pub fn enqueue_lockless(&mut self, dataref: EnqueueParam<T>) -> bool {
+    pub fn enqueue_lockless(&mut self, data: T) -> (QueuedData<T>, bool) {
 
-
+        let queued_data: QueuedData<T> = QueuedData::new(data);
 
         // first, try to acquire the queue lock. 
         match self.qref.queue.try_lock() {
@@ -285,18 +384,34 @@ impl<T> DFQueueProducer<T> {
                 // 1) move all the previously-backlogged data into the inner queue
                 // 2) push the actual `dataref` onto the queue
                 innerq.append(&mut self.local_backlog);
-                innerq.push_back(Arc::new(QueuedData::new(dataref.clone()))); 
-                true
+                innerq.push_back(queued_data.clone()); 
+
+                // TODO: remove completed items from the queue
+
+                (queued_data, true)
             },
             None => {
                 // if we can NOT acquire it:
                 // add the given `dataref` to this DFQueueRef's producer-local backlog queue
-                self.local_backlog.push_back(Arc::new(QueuedData::new(dataref.clone())));
-                false
+
+                // println!("enqueue_lockless: backlogging data: {:?}", queued_data);
+
+                self.local_backlog.push_back(queued_data.clone());
+
+                (queued_data, false)
             }
         }
-
     }
+
+
+    /// A blocking call that flushes this `DFQueueProducer`'s backlog queue to the actual queue.
+    ///
+    /// Note: this is NOT IRQ-safe.
+    pub fn flush_backlog(&mut self) {
+        let mut innerq = self.qref.queue.lock(); // blocking call
+        innerq.append(&mut self.local_backlog);
+    }
+
 
 }
 
@@ -335,43 +450,52 @@ mod test {
 
 
         let mut thr_p = thread::spawn( move || {
-            let original_data: Vec<Arc<usize>> = vec![Arc::new(1), Arc::new(2), Arc::new(3), Arc::new(4), Arc::new(5)];
+            let original_data: Vec<usize> = vec![1, 2, 3, 4, 5];
 
             for i in 1..20 {
                 for elem in original_data.iter() {
-                    queue_prod.enqueue_lockless(elem.clone());
+                    queue_prod.enqueue_lockless(*elem);
                 }
-
-                let queued_data: Arc<usize> = Arc::new(255);
-                queue_prod.enqueue_lockless(queued_data);
             }
+            
+            let (queued_data, backlogged) = queue_prod.enqueue_lockless(256);
+            println!("prod1: queued_data = {:?}, backlogged?: {}", queued_data, backlogged);
+
+            queue_prod.flush_backlog();
         } );
 
          let mut thr_p2 = thread::spawn( move || {
-            let original_data: Vec<Arc<usize>> = vec![Arc::new(10), Arc::new(11), Arc::new(12), Arc::new(13), Arc::new(14)];
+            let original_data: Vec<usize> = vec![10, 11, 12, 13, 14];
 
             for i in 1..20 {
                 for elem in original_data.iter() {
-                    queue_prod2.enqueue_lockless(elem.clone());
+                    queue_prod2.enqueue_lockless(*elem);
                 }
-
-                let queued_data: Arc<usize> = Arc::new(512);
-                queue_prod2.enqueue_lockless(queued_data);
             }
+
+            let (queued_data, backlogged) = queue_prod2.enqueue_lockless(512);
+            println!("prod2: queued_data = {:?}, backlogged?: {}", queued_data, backlogged);
+
+            queue_prod2.flush_backlog();
         } );
 
         let mut thr_c = thread::spawn( move || {
             loop {
                 let mut val = queue_cons.peek_locking();
                 if let Some(v) = val {
-                    println!("peeked: {:?}", v);
+                    println!("peeked: {:?}, value={}", v, *v);
                     v.mark_completed();
+                }
+                else {
+                    // println!("\nDumping queue: \n  {:?}", queue_cons.qref.queue);
                 }
             }
         } );
 
         thr_p.join();
+        println!("producer 1 done");
         thr_p2.join();
+        println!("producer 2 done");
         thr_c.join();
     }
 }
